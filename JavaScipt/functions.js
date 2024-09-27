b5f05cf1a6f54f7b574e9f33a0e7c4a9c28d52e4
Tasir Mahtab Haque <170193862+angkonn@users.noreply.github.com>
2024-09-27 15:53:19 +0600

    Create functions.js

---
+++ JavaScipt/functions.js
@@ -0,0 +1,21 @@
+function nice(name) {
+    console.log("Hey "+name + " You're nice");
+}
+nice("Angkon");
+
+
+function sum(a,b) {
+    
+    return a+b;
+    
+}
+result = sum(6,7);
+console.log("The result is: "+ result);
+
+// arrow function: 
+const func1=(x)=>{
+    console.log("I am an arrow function",x);
+}
+func1(32);
+func1(92);
+func1(222);
