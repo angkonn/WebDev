80e0d8a8e3791e73603a12a07896ddeadb770c7c
Tasir Mahtab Haque <170193862+angkonn@users.noreply.github.com>
2024-09-27 15:28:12 +0600

    Create conditionals.js

---
+++ JavaScipt/conditionals.js
@@ -0,0 +1,43 @@
+console.log("Hello I am conditional tutorial")
+
+let age = 45;
+let grace =2;
+
+if((age+grace)>18){
+    console.log("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}
+
+a=6;
+b=8;
+let c = a>b ? (a-b) : (b-a);
+console.log(c);
+// this means c = (a-b) if a is greater than b but as it is false, then c = (b-a) 
+
+// assigment operators:
+// =     x=y
+// +=    x=x+y
+// -=    x=x-y
+// *=    x=x*y
+// /=    x=x/y
+// %=    x=x%y
+// **=   x=x**y or (x^y)
+
+
+// Comparison Operators:
+// ==: Equal to (compares values, ignores type).
+// ===: Strict equal to (compares values and type).
+// !=: Not equal to (compares values).
+// !==: Strict not equal to (compares values and type).
+// >: Greater than.
+// <: Less than.
+// >=: Greater than or equal to.
+// <=: Less than or equal to.
+
+
+// Logical Operators:
+// &&: Logical AND (returns true if both conditions are true).
+// ||: Logical OR (returns true if at least one condition is true).
+// !: Logical NOT (inverts the boolean value).
