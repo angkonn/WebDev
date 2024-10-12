567e5a9e2eaa0972d97ed4f1cff08fbf8942cdf6
Tasir Mahtab Haque <170193862+angkonn@users.noreply.github.com>
2024-10-12 09:52:01 +0600

    Create FaultyCalculator.js

---
+++ JavaScript/Faulty Calculator/FaultyCalculator.js
@@ -0,0 +1,31 @@
+// Create a faulty calculator using JavaScript
+
+// This faulty calculator does following:
+// 1. It takes two numbers as input from the user
+// 2. It perfoms wrong operations as follows:
+// + ----> -
+// *----> +
+// - ----> /
+// / ----> **
+// It performs wrong operation 10% of the times
+
+let random= Math.random();
+let a=prompt("Enter first number: ")
+let b=prompt("Enter second number: ")
+let c=prompt("Enter operation: ")
+let obj={
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**",
+}
+
+if (random>0.1){
+    // Perform Correct Calculation:
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
+else{
+    // perform wrong calculation:
+    c=obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
