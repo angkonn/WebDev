d8ac58c89d39eb57fc6eb2f2511c7cc771541b27
Tasir Mahtab Haque <170193862+angkonn@users.noreply.github.com>
2024-10-12 13:38:24 +0600

    Create script.js

---
+++ JavaScript/DOM (Document Object Model/script.js
@@ -0,0 +1,38 @@
+// console.log("Hello World")
+
+// Go to inspect and then type these in the console section:
+
+// let cont = document.body.childNodes[1]
+
+// cont
+
+// cont.firstChild
+
+// cont.lastChild
+
+// cont.firstElementChild
+
+// cont.lastElementChild.style.color="red"
+
+// cont.lastElementChild.style.backgroundcolor="green"
+
+// cont.lastElementChild.style.backgroundColor="green"
+
+// cont.lastElementChild.parentElement
+
+// document.body.firstElementChild
+
+// document.body.firstElementChild.childNodes
+
+// document.body.firstElementChild.children
+
+// document.body.firstElementChild.childr
+
+// document.body.firstElementChild.children[0]
+
+// document.body.firstElementChild.children[1]
+
+// document.body.firstElementChild.children[4].previousElementSibling 
+
+// document.body.firstElementChild.children[4].previousSibling 
+
