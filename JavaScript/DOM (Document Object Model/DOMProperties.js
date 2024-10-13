b14f9ff6421e96f83549b8573a5a2ceafdaf45e1
Tasir Mahtab Haque <170193862+angkonn@users.noreply.github.com>
2024-10-13 11:41:13 +0600

    Create DOMProperties.js

---
+++ JavaScript/DOM (Document Object Model/DOMProperties.js
@@ -0,0 +1,21 @@
+
+// document.querySelector(".container").innerHTML
+
+// document.querySelector(".container").innerText
+
+// document.querySelector(".box").innerText
+
+// document.querySelector(".box").innerHTML
+
+// document.querySelector(".container").outerHTML
+
+// document.querySelector(".box").innerHTML="Hey I am Angkon"
+
+// document.querySelector(".box2").hasAttribute="style"
+
+// document.querySelector(".box").attributes
+
+// to design any website: 
+// document.designMode="on"
+
+// document.querySelector('.box2').dataset
