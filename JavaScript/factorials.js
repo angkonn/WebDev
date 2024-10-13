c6551737bed8490aafa5ba69c3af3ccce7cc8481
Tasir Mahtab Haque <170193862+angkonn@users.noreply.github.com>
2024-10-13 09:27:11 +0600

    Update factorials.js

--- JavaScript/factorials.js
+++ JavaScript/factorials.js
@@ -1,22 +1,39 @@
-// using reduce method:
 let a=6
-function factorial(number){
+
+// reduce method: 
+function factorial (number){
     let arr = Array.from(Array(number+1).keys())
-    console.log(arr.slice(1,))
-    let c=arr.slice(1,).reduce((a,b)=>a*b)
-    return c
+    // Array.from : Creates an array from array like object
+    // Array.from(Array(number+1).keys()) : Number + 1 porjonto zotogulo keys ache shobgulo niye array creation
+    console.log(arr)
+    console.log(arr.slice(1,))   // (1 theke shuru hobe)
+    c= arr.slice(1,).reduce((a,b)=>{
+        return a*b 
+    }) 
+    console.log(c)
+   
 }
 
-
-// using for loop:
+// for loop:
 function facfor(number){
-    let fac=1;
-    for (let index = 1; index <=number; index++) {
-       fac=fac*index
-    
+    let n=1
+    for(let i=1;i<=number;i++){
+        n=n*i
     }
-    return fac
-
+    return n 
 }
-console.log(factorial(a))
+
+// using if else:
+function facif(number){
+   if (number<=1){
+    return 1
+   }
+   else{
+    return number*facif(number-1)
+   }
+    
+   
+}
+factorial(a)
 console.log(facfor(a))
+console.log(facif(a))
